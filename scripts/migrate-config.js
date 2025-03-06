aa4eb3c69d4d41e024ff324723afecc72e3a9785
FlintSH <me@fl1nt.dev>
2025-03-05 21:46:37 -0500

    fix a bug with default config in migration script

--- scripts/migrate-config.js
+++ scripts/migrate-config.js
@@ -3,7 +3,7 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 const DEFAULT_CONFIG = {
-  version: '1.0.0',
+  version: '1.1.0',
   settings: {
     general: {
       setup: {
@@ -46,7 +46,27 @@ const DEFAULT_CONFIG = {
     appearance: {
       theme: 'dark',
       favicon: null,
-      customColors: {},
+      customColors: {
+        card: '222.2 84% 4.9%',
+        ring: '212.7 26.8% 83.9%',
+        input: '217.2 32.6% 17.5%',
+        muted: '217.2 32.6% 17.5%',
+        accent: '217.2 32.6% 17.5%',
+        border: '217.2 32.6% 17.5%',
+        popover: '222.2 84% 4.9%',
+        primary: '210 40% 98%',
+        secondary: '217.2 32.6% 17.5%',
+        background: '222.2 84% 4.9%',
+        foreground: '210 40% 98%',
+        destructive: '0 62.8% 30.6%',
+        cardForeground: '210 40% 98%',
+        mutedForeground: '215 20.2% 65.1%',
+        accentForeground: '210 40% 98%',
+        popoverForeground: '210 40% 98%',
+        primaryForeground: '222.2 47.4% 11.2%',
+        secondaryForeground: '210 40% 98%',
+        destructiveForeground: '210 40% 98%',
+      },
     },
     advanced: {
       customCSS: '',
